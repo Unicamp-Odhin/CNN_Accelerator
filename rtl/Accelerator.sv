de13ab425d3d1e12f2ea3dfb7dd0392213978208
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-08-20 18:54:03 -0300

    Adicionando sinais para suporte a Burst

--- rtl/Accelerator.sv
+++ rtl/Accelerator.sv
@@ -3,37 +3,43 @@ module Accelerator #(
     parameter ADDR_WIDTH = 32,
     parameter CTRL_WIDTH = 32
 ) (
-    input  logic clk,
-    input  logic rst_n,
+    input  logic                     clk,
+    input  logic                     rst_n,
 
     // Control port
-    output logic control_cyc_o,
-    output logic control_stb_o,
-    output logic control_we_o,
-    input  logic control_ack_i,
-    output logic [ADDR_WIDTH - 1: 0] control_addr_o,
-    output logic [CTRL_WIDTH - 1: 0] control_mosi_o,
-    input  logic [CTRL_WIDTH - 1: 0] control_miso_i,
+    input  logic                     control_cyc_o,
+    input  logic                     control_stb_o,
+    input  logic                     control_we_o,
+    output logic                     control_ack_i,
+    input  logic [ADDR_WIDTH - 1: 0] control_addr_o,
+    input  logic [CTRL_WIDTH - 1: 0] control_mosi_o,
+    output logic [CTRL_WIDTH - 1: 0] control_miso_i,
+    input  logic               [2:0] control_cti_o,
+    input  logic               [1:0] control_bte_o,
 
     output logic done_interrupt_o,
 
     // Cache port
-    output logic cache_cyc_o,
-    output logic cache_stb_o,
-    output logic cache_we_o,
-    input  logic cache_ack_i,
+    output logic                     cache_cyc_o,
+    output logic                     cache_stb_o,
+    output logic                     cache_we_o,
+    input  logic                     cache_ack_i,
     output logic [ADDR_WIDTH - 1: 0] cache_addr_o,
     output logic [DATA_WIDTH - 1: 0] cache_mosi_o,
     input  logic [DATA_WIDTH - 1: 0] cache_miso_i,
+    output logic               [2:0] cache_cti_o,
+    output logic               [1:0] cache_bte_o,
 
-    // Ceps Buffer port
-    output logic ceps_cyc_o,
-    output logic ceps_stb_o,
-    output logic ceps_we_o,
-    input  logic ceps_ack_i,
-    output logic [ADDR_WIDTH - 1: 0] ceps_addr_o,
-    output logic [DATA_WIDTH - 1: 0] ceps_mosi_o,
-    input  logic [DATA_WIDTH - 1: 0] ceps_miso_i
+    // Samples Buffer port
+    output logic                     samples_cyc_o,
+    output logic                     samples_stb_o,
+    output logic                     samples_we_o,
+    input  logic                     samples_ack_i,
+    output logic [ADDR_WIDTH - 1: 0] samples_addr_o,
+    output logic [DATA_WIDTH - 1: 0] samples_mosi_o,
+    input  logic [DATA_WIDTH - 1: 0] samples_miso_i,
+    output logic               [2:0] samples_cti_o,
+    output logic               [1:0] samples_bte_o
 );
     
 endmodule
